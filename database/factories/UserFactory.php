a8fc9e994f57ae3ba9e22cebfa8093ade9b80e3f
김태훈 <rlaxognsk@plani.co.kr>
2021-11-02 10:06:27 +0900

    change: 회원 name 필드 삭제

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,7 +14,6 @@ class UserFactory extends Factory
     {
         return [
             'account' => $this->faker->unique()->name(),
-            'name' => $this->faker->name(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
