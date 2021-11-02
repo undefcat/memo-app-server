629f9efff799095d24cafc5e21d2e1f314f95540
undefcat <undefcat@gmail.com>
2021-11-02 14:15:06 +0900

    chore: 프로젝트 디렉터리 구조 변경 및 unused import 제거

--- tests/Feature/User/UserSignUpTest.php
+++ tests/Feature/User/UserSignUpTest.php
@@ -1,10 +1,9 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\User;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
